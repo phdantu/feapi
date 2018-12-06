25befc33af7a9c42bb454abbf32017e025dd553f
Danton Coelho <dantonclp@gmail.com>
2018-12-06 11:31:09 -0200

    First Commit

---
+++ ci/application/controllers/pagina.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Pagina extends CI_Controller {
+
+    function __contruct(){
+		parent::__construct();
+		//$this->load->helper('url');
+    }
+
+	public function index()
+	{
+		$dados['titulo'] = "Cadastro de Livros";
+		$this->load->view('home',$dados);
+	}
+
+	public function tabela()
+	{
+		$dados['titulo'] = "Tabela de Livros";
+		$this->load->view('home',$dados);
+	}
+}
